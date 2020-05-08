7ba2767e70d8cce74ec259f362f9ab65a51ff36b
Jihyun247 <wlguslove12345@gmail.com>
2020-05-08 23:53:48 +0900

    SecondWeek_Exercise1_incomp

---
+++ IOS_SecondWeek_Seminar_Lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_SecondWeek_Seminar_Lab1
+//
+//  Created by 김지현 on 01/05/2020.
+//  Copyright © 2020 김지현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
